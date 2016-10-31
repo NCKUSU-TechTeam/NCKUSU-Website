42ea8d7792322e11bf8dfa248ad6bcd60d7327b1
kevinbird61 <kevinbird61@gmail.com>
2016-10-31 19:18:57 +0800

    Change 500 to 200

--- urlHandler.js
+++ urlHandler.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
 function main_page(response){
-  response.writeHead(500, {"Content-Type": "text/html"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   // Get from a html file.
   // readFile => async , readFileSync => sync
   var html = fs.readFileSync('src/index.html','utf8');
@@ -10,7 +10,7 @@ function main_page(response){
 }
 
 function intro_page(response){
-  response.writeHead(500, {"Content-Type": "text/html"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   // Get from a html file.
   var html = fs.readFileSync('src/intro.html','utf8');
   response.write(html);
