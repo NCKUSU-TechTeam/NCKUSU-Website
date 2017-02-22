eb62f2235a51f15952714cf5da5022f71d907d74
kevinbird61 <kevinbird61@gmail.com>
2017-02-22 09:21:24 +0800

    Upload new structure.

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-var httpserver = require("http");
+const express = require('express');
+const app = express();
+const path = require('path');
+const moment = require('moment');
+const bodyParser = require('body-parser');
+/* Redirect views path */
+app.set('views',path.join(__dirname,'src/views'));
+/* Setting static directory - image use */
+app.use(express.static('src/images'));
+app.use(bodyParser.urlencoded({extended: false}));
+/* Setting view engine as ejs */
+app.set('view engine','ejs');
+const server = require('http').createServer(app);
 var url = require("url");
-var router = require("./router");
 
-function onRequest(request,response){
-  // Doing thing in each request
-  var pathname = url.parse(request.url).pathname;
-  console.log("Request for "+pathname+" received.");
-  router.parsing(pathname,response);
-}
+// URL maker
+app.get('/',function(request,response){
+    // get all path
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + " received.");
+    response.render('index',{
+        title: 'NCKU-su'
+    });
+});
 
-httpserver.createServer(onRequest).listen(8080);
-console.log("Server has started!");
+app.get('/activity',function(request,response){
+    response.end("activity");
+});
+
+app.get('/department',function(request,response){
+    response.end("department");
+});
+
+app.get('/about',function(request,response){
+    response.end('about')
+});
+
+// Listen url request
+server.listen(process.env.npm_package_config_port, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    // logger.record('io.render',"[io.render] Example app listening at "+host+" : "+port);
+    console.log("[Ncku Website start] Open Web service at ["+host+" : "+port+"] , time: " + moment().format());
+});
